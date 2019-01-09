991404a7a4429233d83fe5f453cd9814f346cf64
Khangarid <khangarid.davaadorj@gmail.com>
2019-01-08 16:33:05 -0800

    Updates

---
+++ precache-manifest.caa776a17d1bbef95938bf03ba73b733.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d0c8a9b727e0626383b",
+    "url": "/static/js/main.9d0c8a9b.chunk.js"
+  },
+  {
+    "revision": "8316f41727e5b4f8cee9",
+    "url": "/static/js/1.8316f417.chunk.js"
+  },
+  {
+    "revision": "9d0c8a9b727e0626383b",
+    "url": "/static/css/main.427b590e.chunk.css"
+  },
+  {
+    "revision": "ca97eb956a08227193f0eaf770cc1cbf",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.943d3e1a09f702cc67a5f5ee3ccf06fe.js"
+  "/precache-manifest.caa776a17d1bbef95938bf03ba73b733.js"
 );
 
 workbox.clientsClaim();
